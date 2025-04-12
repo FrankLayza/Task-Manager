1b22f360526de222e77463b6129f494486698f84
Frank Anthony <frankez478@gmail.com>
2025-04-12 06:23:57 +0100

    implemented the other functionalites, like filtering active and completed tasks

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import Dark from './assets/images/bg-desktop-dark.jpg';
 import Form from './Components/Form';
+import {useEffect, useState} from 'react';
 function App() {
+  const [toggle, setToggle] = useState(false);
+
+
+  const toggleTheme = () => {
+    setToggle((prev) => !prev)
+  }
+
+  useEffect(() => {
+    document.body.style.backgroundColor = toggle ? 'hsl(0, 0%, 98%)' : 'hsl(235, 21%, 11%)';
+  }, [toggle])
   return (
     <>
-      <div className="flex justify-center items-center flex-col bg-cover bg-center h-[30vh]" style={{backgroundImage: `url(${Dark})`}}>
-        <Form />
+      <div className="flex justify-center items-center flex-col bg-cover bg-center h-[40vh]" style={{backgroundImage: `url(${Dark})`}}>
+        <Form toggleTheme={toggleTheme} toggle={toggle}/>
       </div>
     </>
   );
--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,27 +1,153 @@
-import { useState } from 'react';
-import {FaSun, FaMoon} from 'react-icons/fa';
+import { useState, useEffect } from "react";
+import { FaSun, FaMoon, FaTimes } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
 
-const Form = () => {
-    const [input, setInput] = useState('')
+const Form = ({ toggleTheme, toggle }) => {
+  const [input, setInput] = useState("");
+  const [todoList, setTodo] = useState([]);
+  const [visibleList, setVisible] = useState([]);
+  const [count, setCount] = useState(0);
 
-    return (
-        <>
-            <div className=" p-20 h-full w-[500px]">
-                <div className="flex justify-between mb-5">
-                    <h2 className="text-white text-4xl font-normal">TODO</h2>
-                    <button><FaSun className='text-white'/></button>
-                </div>
+  const showAll = () => {
+    setVisible(todoList);
+  };
 
-                <div className='flex bg-dark-300 w-full p-3'>
-                    <label className='inline-flex items-center cursor-pointer mx-2'>
-                        <input type="checkbox" className='sr-only peer' />
-                        <div className='rounded-full border-gray-600 w-5 h-5 border-2 peer-checked:bg-blue-400 transition'></div>
-                    </label>
-                    <input value={input} className=" text-gray-400" type="text" placeholder="Create a new todo..."/>
-                </div>
+  const addTask = () => {
+    const newTodo = { id: Date.now(), text: input, completed: false };
+    const updatedTodo = [newTodo, ...todoList];
+    setTodo(updatedTodo);
+    setVisible(updatedTodo);
+    setCount(count + 1);
+    setInput("");
+  };
+
+  const removeTask = (id) => {
+    const updated = todoList.filter((todo) => todo.id !== id);
+    setTodo(updated);
+    setVisible(updated);
+    setCount(count - 1);
+  };
+  
+  const toggleCompleted = (id) => {
+    const updated = (
+      todoList.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+    setTodo(updated)
+    setVisible(updated)
+  };
+
+  const activeTask = () => {
+    setVisible(todoList.filter((todo) => !todo.completed));
+  };
+  
+  const filterCompleted = () => {
+    setVisible(todoList.filter((todo) => todo.completed));
+  };
+
+  useEffect(() => {
+    setVisible(todoList);
+  }, []);
+  return (
+    <>
+      <div className=" p-20 h-full w-[500px]">
+        <div className="flex justify-between mb-5">
+          <h2 className="text-white text-4xl font-normal">TODO</h2>
+          <button onClick={toggleTheme}>
+            {toggle ? (
+              <FaSun className="text-white cursor-pointer" />
+            ) : (
+              <FaMoon className="text-white cursor-pointer" />
+            )}
+          </button>
+        </div>
+
+        <div
+          className={`flex ${toggle ? "bg-white" : "bg-dark-300"}  w-full p-3`}
+        >
+          <label className="inline-flex items-center cursor-pointer mx-2">
+            <input type="checkbox" className="sr-only peer" />
+            <div className="rounded-full border-gray-600 w-5 h-5 border-1 peer-checked:bg-gradient-to-r from-gradientBlue to-gradientPurple transition"></div>
+          </label>
+          <input
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            className={`${
+              toggle ? "text-black" : "text-gray-400"
+            } text-[18px] outline-none border-none`}
+            type="text"
+            placeholder="Create a new todo..."
+          />
+          <button
+            className="ml-auto"
+            onClick={() => {
+              addTask();
+            }}
+          >
+            <IoSend
+              className={`${
+                toggle ? "text-dark-300" : "text-white"
+              } cursor-pointer`}
+            />
+          </button>
+        </div>
+
+        {/* todolist */}
+        <div className="mt-6">
+          {visibleList.map((todo) => (
+            <div
+              key={todo.id}
+              className={`flex ${
+                toggle ? "bg-white" : "bg-dark-300"
+              } w-full p-3`}
+            >
+              <label className="inline-flex items-center cursor-pointer mx-2">
+                <input
+                  type="checkbox"
+                  className="sr-only peer"
+                  checked={todo.completed}
+                  onChange={() => toggleCompleted(todo.id)}
+                />
+                <div className="rounded-full border-gray-600 w-5 h-5 border-1 peer-checked:bg-gradient-to-r from-gradientBlue to-gradientPurple transition"></div>
+              </label>
+              <span
+                className={`${toggle ? "text-black" : "text-gray-400"} ${
+                  todo.completed ? "line-through opacity-50" : ""
+                }`}
+              >
+                {todo.text}
+              </span>
+              <button className="ml-auto" onClick={() => removeTask(todo.id)}>
+                <FaTimes className="text-darkGrayishBlue cursor-pointer" />
+              </button>
             </div>
-        </>
-    )
-}
+          ))}
+          <div
+            className={`flex justify-between text-[10px] overflow-hidden w-full ${
+              toggle ? "bg-white text-black" : "bg-dark-300 text-gray-400"
+            } p-3`}
+          >
+            <div>{count} items left</div>
+            <div className="flex">
+              <button className="ml-2 cursor-pointer" onClick={showAll}>
+                All
+              </button>
+              <button className="mx-2 cursor-pointer" onClick={activeTask}>
+                Active
+              </button>
+              <button className="cursor-pointer" onClick={filterCompleted}>
+                Completed
+              </button>
+            </div>
+            <div>
+              <p>Clear Completed</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Form;
